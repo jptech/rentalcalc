42e3626e472b922a00984b7fca49c29234c7d8a3
Parker Mitchell <parker@ieee.org>
2025-10-05 07:54:14 -0700

    deployment pt1

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://jptech.github.io/rentalcalc/",
   plugins: [react(), tailwindcss()],
 });
